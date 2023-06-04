5ac651f2e428290f223307601efa66ed22657f4d
dayojiboye <my@personal>
2023-06-04 16:35:00 +0100

    fix: deleted declarations.d.ts and env.d.ts files

--- react-app-env.d.ts
+++ react-app-env.d.ts
@@ -2,3 +2,13 @@ declare module "*.png";
 declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
+declare module "@env" {
+	export const OPEN_WEATHER_KEY: string;
+	export const BASE_URL: string;
+}
+declare module "*.svg" {
+	import React from "react";
+	import { SvgProps } from "react-native-svg";
+	const content: React.FC<SvgProps>;
+	export default content;
+}
